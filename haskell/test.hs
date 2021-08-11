91403e2d9b7f8fdc28e9e243c3e2c4dffa3e497e
Will Martin <wcmartin01@gmail.com>
2021-08-11 17:49:20 -0500

    factorial

--- haskell/test.hs
+++ haskell/test.hs
@@ -1,3 +1,16 @@
 doubleMe x = x + x
 
-doubleUs x y = x*2 + y*2   
+doubleUs x y = x*2 + y*2
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial a = a * factorial (a-1)
+
+factorial' :: (Integral b) => b -> b
+factorial' b
+        | b == 0 = 1
+        | otherwise = b * factorial' (b-1)
